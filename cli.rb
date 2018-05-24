7ec6118197ba1bdc14566a4b7e4c9ee38564b7d6
akisame338 <akisame338@gmail.com>
2018-05-25 01:06:16 +0900

    取得したissuesを以下の内容のCSV形式で出力
    ・titleの先頭30文字
    ・bodyの先頭50文字
    ・html_url

--- cli.rb
+++ cli.rb
@@ -1,10 +1,21 @@
 #!/usr/bin/env ruby
 
+require 'csv'
+require 'json'
 require 'rest-client'
 
 def main
   url = 'https://api.github.com/repos/rails/rails/issues'
   response = RestClient.get url
+  issues = JSON.parse response.body
+
+  csv_data = CSV.generate({:force_quotes => true}) do |csv|
+    issues.each do |issue|
+      csv << [issue['title'][0, 30], issue['body'][0, 50], issue['html_url']]
+    end
+  end
+
+  puts csv_data
 end
 
 if __FILE__ == $0
